c5b51845a119b4dbdbbd5d45035f1a49ade22b7c
AbhishekAmeta7 <abhishekameta55@gmail.com>
2025-02-18 15:45:55 +0530

    Print Linkedlist Elements

---
+++ LinkedList/PrintLinkedlist.java
@@ -0,0 +1,27 @@
+public class PrintLinkedlist {
+    static class Node{
+        int value;
+        Node next;
+
+        Node(int value){
+            this.value = value;
+        }
+    }
+    public static void main(String[] args) {
+       Node a = new Node(11);
+       Node b = new Node(22);
+       Node c = new Node(33);
+       Node d = new Node(44);
+       Node e = new Node(55);
+       a.next = b;
+       b.next = c;
+       c.next = d;
+       d.next = e;
+       e.next = null;
+       Node temp = a;
+       while(temp!=null) {
+         System.out.print(temp.value + " ");
+         temp = temp.next;  // Move to the next node in the linked list.
+       }
+    }
+}
