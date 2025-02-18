fc18c3956b005d26a7e6aa9b04624f2cced72a6c
AbhishekAmeta7 <abhishekameta55@gmail.com>
2025-02-18 11:44:37 +0530

    LinkedList Create

---
+++ LinkedList/CreateNode.java
@@ -0,0 +1,12 @@
+public class CreateNode{
+    static class Node{
+        int value;
+        Node next;
+        Node(int value){
+            this.value = value;
+        }
+    }
+    public static void main(String[] args) {
+       
+    }
+}
