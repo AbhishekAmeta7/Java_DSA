bdfcef603a0cd4006c728345382bed40eceb5f0d
AbhishekAmeta7 <abhishekameta55@gmail.com>
2025-02-27 20:56:11 +0530

    Delete Specific Node From LinkedList

--- LinkedList/DeleteNode.java
+++ LinkedList/DeleteNode.java
@@ -1,48 +1,71 @@
+import java.util.Scanner;
+
 public class DeleteNode {
     static class Node{
-        int data;
-        Node next;
-        Node(int data){
-            this.data = data;
-        }
-    }
-    static class Linkedlist {
-        public void display(Node head){
+		int data;
+		Node next;
+		
+		public Node(int data) {
+			this.data = data;
+		}
+	}
+	
+	static class Linkedlist{
+		public void display(Node head){
 			Node curNode = head;
 			while(curNode != null) {
-				System.out.print(curNode.data+" ");
+				System.out.print(curNode.data + " ");
 				curNode = curNode.next;
 			}
+			System.out.println();
 		}
-		public void removeNode(Node head,Node node) {
-			Node slow = head;
-            Node fast = head;
-            while(fast != null && fast.next!=null){
-            	if(slow.next==node){
-                    slow.next = slow.next.next;
-                }
-                slow = slow.next;
-                fast = fast.next.next;
-            }
-            System.out.println();
-            display(head);
-		}  
-    }
-    public static void main(String[] args) {
-        Node a = new Node(4);
-		Node b = new Node(5);
-		Node c = new Node(1);
-		Node d = new Node(9);
-		Node e = new Node(50);
 		
-		a.next = b;
-		b.next = c;
-		c.next = d;
-		d.next = e;
-		e.next = null;
+		public Node removeNode(Node head, int value) {
+			if (head == null) return null;
+			if (head.data == value) return head.next;
+			
+			Node current = head;
+			while (current.next != null) {
+				if (current.next.data == value) {
+					current.next = current.next.next;
+					break;
+				}
+				current = current.next;
+			}
+			return head;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of nodes: ");
+		int n = sc.nextInt();
+		
+		Node head = null, tail = null;
+		System.out.println("Enter the node values: ");
+		for (int i = 0; i < n; i++) {
+			int data = sc.nextInt();
+			Node newNode = new Node(data);
+			if (head == null) {
+				head = newNode;
+				tail = newNode;
+			} else {
+				tail.next = newNode;
+				tail = newNode;
+			}
+		}
 		
 		Linkedlist ll = new Linkedlist();
-		ll.display(a);
-		ll.removeNode(a, c);
-    }    
+		System.out.print("Initial Linked List: ");
+		ll.display(head);
+		
+		System.out.print("Enter the value of the node to remove: ");
+		int valueToRemove = sc.nextInt();
+		
+		head = ll.removeNode(head, valueToRemove);
+		System.out.print("Linked List after removal: ");
+		ll.display(head);
+		
+		sc.close();
+	}    
 }
